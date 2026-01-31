6d05cd39eb95f5928b81e89d2962bf93d983a26a
cyberelpiro23-lang <cyberelpiro23@gmail.com>
2026-01-31 07:43:32 -0300

    Add files via upload

---
+++ personaje.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+export class Character {
+    constructor(scene, color, xPos) {
+        this.mesh = new THREE.Group();
+        const body = new THREE.Mesh(
+            new THREE.CapsuleGeometry(0.5, 1, 4, 8),
+            new THREE.MeshStandardMaterial({ color: color })
+        );
+        body.position.y = 1;
+        this.mesh.add(body);
+        
+        const nose = new THREE.Mesh(new THREE.BoxGeometry(0.2, 0.2, 0.5), new THREE.MeshStandardMaterial({ color: 0x000000 }));
+        nose.position.set(0, 1.2, 0.5);
+        this.mesh.add(nose);
+        
+        this.mesh.position.set(xPos, 0, 0);
+        scene.add(this.mesh);
+    }
+
+    mover(x, z) {
+        this.mesh.position.x += x * 0.15;
+        this.mesh.position.z += z * 0.15;
+        if (x !== 0 || z !== 0) this.mesh.rotation.y = Math.atan2(x, z);
+    }
+}
